553d272627f37176cf1d143b99002abda5ac3610
iowi479 <simon@alle-schindlers.de>
2025-01-25 22:59:05 +0100

    small fix

--- env/.config/nvim/lua/plugins.lua
+++ env/.config/nvim/lua/plugins.lua
@@ -14,7 +14,6 @@ return {
 
 			-- Add/delete/replace surroundings (brackets, quotes, etc.)
 			--
-			--
 			-- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
 			-- - sd'   - [S]urround [D]elete [']quotes
 			-- - sr)'  - [S]urround [R]eplace [)] [']
